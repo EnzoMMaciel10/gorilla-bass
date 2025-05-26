5783fe75724a3a7b3e9767a7b9ff468e96e976c8
EnzoMMaciel10 <enzomontmaciel@gmail.com>
2025-05-25 21:23:01 -0300

    criação das variáveis para controle da interface: vida do gorila, humanos, botões e log de batalha .

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const gorilaVidaSpan = document.getElementById('gorilaVida');
+const humanosRestantesSpan = document.getElementById('humanosRestantes');
+const ataquesFeitosSpan = document.getElementById('ataquesFeitos');
+const logDiv = document.getElementById('log');
+const campoHumanos = document.getElementById('campoHumanos');
+const btnAtacar = document.getElementById('btnAtacar');
+const btnDefender = document.getElementById('btnDefender');
+const btnCurar = document.getElementById('btnCurar');
+const btnRecomecar = document.getElementById('btnRecomecar');
+
